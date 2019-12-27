36870842ece2d529b82e3ad3d535b4030248bf4c
Luis Fernando <fernando04_software@outlook.com>
2019-12-27 02:48:46 -0600

    New File - Hello World
    Archivo .cpp que contiene codigo para la creacion del 'Hello World'

---
+++ basic/hello_world.cpp
@@ -0,0 +1,9 @@
+/* Hello World en C++ */
+
+#include "iostream"    // Añadimos libreria para nuestro programa
+using namespace std;
+
+int main(){
+    cout << "Hello World in C++";   // Instruccion que nos permite imprimir texto en consola
+    return 0; 
+}
